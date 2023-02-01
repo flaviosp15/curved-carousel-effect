a9acb389a5c322e9d5e3aef6dbe865ff68a09bd7
Flavio Silva <flaviosp15@outlook.com>
2023-01-31 23:32:17 -0300

    :sparkles: feat: Added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './Header.js';
+import Hero from './Hero.js';
+import Carousel from './Carousel.js';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
